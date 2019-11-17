c8361ecc31bd8abb1007e00f48366ea7a0c0dca5
SHagege <solalhagege@gmail.com>
2019-11-17 11:59:13 -0800

    Updates

---
+++ precache-manifest.c60b574a275a5c25e0bc0962d389f736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e621f3a1680dfcf2b243fbb9c39fa7b",
+    "url": "/satoshi-analytics/index.html"
+  },
+  {
+    "revision": "94e91833330bfe028fec",
+    "url": "/satoshi-analytics/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "44662e62b24c0e1a78c0",
+    "url": "/satoshi-analytics/static/js/2.4b9fb1a3.chunk.js"
+  },
+  {
+    "revision": "94e91833330bfe028fec",
+    "url": "/satoshi-analytics/static/js/main.82d5af48.chunk.js"
+  },
+  {
+    "revision": "7f0ff9893765e277c31f",
+    "url": "/satoshi-analytics/static/js/runtime-main.5268a342.js"
+  }
+]);
