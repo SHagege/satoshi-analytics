6a53830da4420860cde9f4874c7989ffa9539ac9
SHagege <solalhagege@gmail.com>
2019-11-18 10:17:39 -0800

    Updates

---
+++ precache-manifest.33de9e3b91fcd7cf4bf65700ebbb7de2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e60ae32c6e6ac2fa889bfb85aab182d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bca1c8df99fde8c1559",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d3985ca040a44e966f7d",
+    "url": "/static/js/2.5f4ef27a.chunk.js"
+  },
+  {
+    "revision": "1bca1c8df99fde8c1559",
+    "url": "/static/js/main.b76e7ebc.chunk.js"
+  },
+  {
+    "revision": "7ea0b5f92da89145d31e",
+    "url": "/static/js/runtime-main.32f8a958.js"
+  }
+]);
