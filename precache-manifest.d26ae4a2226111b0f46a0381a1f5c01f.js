79d5351bb721e15a3e7a7d8e970de195fcbb70b8
SHagege <solalhagege@gmail.com>
2019-12-05 15:33:41 -0800

    Updates

--- precache-manifest.d26ae4a2226111b0f46a0381a1f5c01f.js
+++ precache-manifest.d26ae4a2226111b0f46a0381a1f5c01f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "093203cb40be521817a25c7be535b88d",
+    "revision": "f58c34a8bdf2ab449c385e8a0149bafa",
     "url": "/satoshi-analytics/index.html"
   },
   {
-    "revision": "f66dbad1ad874bc89490",
+    "revision": "1c34701b9597d899ec1a",
     "url": "/satoshi-analytics/static/css/main.34de6062.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/satoshi-analytics/static/js/2.82eafafc.chunk.js"
   },
   {
-    "revision": "f66dbad1ad874bc89490",
-    "url": "/satoshi-analytics/static/js/main.336511d4.chunk.js"
+    "revision": "1c34701b9597d899ec1a",
+    "url": "/satoshi-analytics/static/js/main.d3c138f8.chunk.js"
   },
   {
     "revision": "55139d8856f69f9f5ee8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/satoshi-analytics/precache-manifest.406e953a9198d71ac7b310ed7e866b53.js"
+  "/satoshi-analytics/precache-manifest.d26ae4a2226111b0f46a0381a1f5c01f.js"
 );
 
 self.addEventListener('message', (event) => {
