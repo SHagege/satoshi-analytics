3e6f822492bc6b4036d31631f72d400894cfb673
SHagege <solalhagege@gmail.com>
2019-12-06 11:14:03 -0800

    Update API source

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -12,7 +12,7 @@ class About extends React.Component {
                     </h1>
                     <h2>
                         <b>Satoshi Analytics is a statistics website focused on Bitcoin<br /><br />
-                            Built with <a href="https://github.com/Blockchair/Blockchair.Support/blob/master/API.md">Blockchair's API </a>and
+                            Built with <a href="https://blockchair.com/api/docs">Blockchair's API </a>and
                             <a href="https://reactjs.org/"> ReactJS</a>. Opensource on <a href="https://github.com/SHagege/satoshi-analytics">GitHub</a>.
                             Created by</b> <a href="https://twitter.com/solalhagege">@solalhagege</a>.
                     </h2>
